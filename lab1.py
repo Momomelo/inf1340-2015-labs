3331b895d38b6e4ce5338b14c4736168d9dcc539
Momomelo <y.kei1ro@gmail.com>
2015-10-01 11:58:22 -0400

    Hardcoded all the things
    There's better ways to do this

--- lab1.py
+++ lab1.py
@@ -18,3 +18,16 @@ def vowel_or_consonant():
     display a message indicating that the letter is a "consonant".
     """
 
+user_letter = raw_input ("Please enter a letter to see if it is a vowel or consenant!").lower()
+if user_letter == "a" or user_letter == "e" or user_letter == "i" or user_letter == "o" or user_letter == "u":
+	print ("vowel")
+elif user_letter == "y":
+	print ("sometimes a voewl")
+elif user_letter == "b" or user_letter == "c" or user_letter == "d" or user_letter == "f" or user_letter == "g" or user_letter == "h" or user_letter == "j" or user_letter == "k" or user_letter == "l" or user_letter == "m" or user_letter == "n" or user_letter == "p" or user_letter == "q" or user_letter == "r" or user_letter == "s" or user_letter == "t" or user_letter == "v" or user_letter == "w" or user_letter == "x" or user_letter == "y" or user_letter == "z":
+	print ("consonant")
+else:
+	print ("That's not a single letter silly!")
+
+#The above code is really silly! bwcause everything is hardcoded.
+#Let's learn how to use the len() function to not make everything so hard coded!
+
